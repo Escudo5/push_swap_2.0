2f6410f408bfc76d8203cdb1e30679c3180d0c5e
Escudo5 <sergiomargar03@gmail.com>
2025-01-07 13:30:41 +0100

    falta acabar el .h y los utils del get index

--- sources/principal/init.c
+++ sources/principal/init.c
@@ -6,12 +6,16 @@
 /*   By: smarquez <smarquez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 11:09:54 by smarquez          #+#    #+#             */
-/*   Updated: 2025/01/07 11:10:33 by smarquez         ###   ########.fr       */
+/*   Updated: 2025/01/07 13:20:52 by smarquez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(int argc, char **argv)
+#include "push_swap.h"
+
+void init_push_swap(t_list **stack_a, t_list **stack_b)
 {
-    t_list *stack_a;
-    t_list *stack_b;
+    int nums;
+    nums = ft_lstsize(*stack_a);
+    get_index(stack_a);
+    if (!num)
 }
---
+++ sources/principal/utils.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smarquez <smarquez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/07 13:21:03 by smarquez          #+#    #+#             */
+/*   Updated: 2025/01/07 13:29:18 by smarquez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int nums_ordered(t_list **stack)
+{
+    t_list *current;
+
+    current = *stack;
+    while (current && current->next)
+    {
+        if (*((int*)current->content) > *((int*)current->next->content))
+            return(0);
+        current = current->next;
+    }
+    return(1);
+}
+
+
+void get_index(t_list **stack)
+{
+    t_list *current;
+    t_list *temp;
+    size_t count;
+
+    current = *stack;
+    while (current != NULL)
+    {
+        count = 0;
+        temp = *stack;
+        while (temp != NULL)
+        {
+            if (*(int*)current->content > *(int*)temp->content)
+                count++;
+            temp = temp->next;
+        }
+        current->index = count;
+    }
+}

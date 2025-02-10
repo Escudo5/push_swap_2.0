ba9e0fb6e9aa5f18743e17a461ad7b341986a156
Escudo5 <sergiomargar03@gmail.com>
2025-02-10 13:22:13 +0100

    modificado moves_check del bonus para coincidiri con rr,ss y rrr

--- sources/bonus/moves_check.c
+++ sources/bonus/moves_check.c
@@ -6,7 +6,7 @@
 /*   By: smarquez <smarquez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 10:27:05 by smarquez          #+#    #+#             */
-/*   Updated: 2025/02/10 12:04:27 by smarquez         ###   ########.fr       */
+/*   Updated: 2025/02/10 13:21:51 by smarquez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,19 +23,19 @@ void	moves_check(t_list **stack_a, t_list **stack_b, char *line)
 	else if (ft_strncmp(line, "rb\n", 3) == 0)
 		rb(stack_b, false);
 	else if (ft_strncmp(line, "rr\n", 3) == 0)
-		rr(stack_a, stack_b, false);
+		rr(stack_a, stack_b);
 	else if (ft_strncmp(line, "rra\n", 4) == 0)
 		rra(stack_a, false);
 	else if (ft_strncmp(line, "rrb\n", 4) == 0)
 		rrb(stack_b, false);
 	else if (ft_strncmp(line, "rrr\n", 4) == 0)
-		rrr(stack_a, stack_b, false);
+		rrr(stack_a, stack_b);
 	else if (ft_strncmp(line, "sa\n", 3) == 0)
 		sa(stack_a, false);
 	else if (ft_strncmp(line, "sb\n", 3) == 0)
 		sb(stack_b, false);
 	else if (ft_strncmp(line, "ss\n", 3) == 0)
-		ss(stack_a, stack_b, false);
+		ss(stack_a, stack_b);
 	else
 		print_error(stack_a);
 }
